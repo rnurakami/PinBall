13e22990e2ab5225dcfe5c9f5ff6c8a75ab696c7
rnurakami <mariamanzini8@gmail.com>
2019-06-30 15:22:43 +0900

    Make user can play on touch Screen

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -9,14 +9,35 @@ public class FripperController : MonoBehaviour {
     private float defaultAngle = 20f;
     private float flickAngle = -20f;
 
-	// Use this for initialization
-	void Start () {
+    private int center = Screen.width / 2;
+
+    // Use this for initialization
+    void Start () {
         this.myHingeJoint = GetComponent<HingeJoint>();
         SetAngle(this.defaultAngle);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        foreach (Touch touch in Input.touches)
+        {
+            bool touchOnTheLeftSide = touch.position.x < center && tag == "LeftFripperTag";
+            bool touchOnTheRightSide = touch.position.x >= center && tag == "RightFripperTag";
+
+            if (touchOnTheLeftSide || touchOnTheRightSide)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    SetAngle(this.flickAngle);
+                }
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+
+        }
 
         if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
         {
