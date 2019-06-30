1bcc49482adbc817f6e0c524b3f26b333ea471be
rnurakami <mariamanzini8@gmail.com>
2019-06-30 11:32:49 +0900

    Display "Game Over" when the ball is out of board

---
+++ Assets/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    private float visiblePosZ = -6.5f;
+
+    private GameObject gameoverText;
+
+    void Start()
+    {
+        this.gameoverText = GameObject.Find("GameOverText");
+    }
+
+    void Update()
+    {
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+
+    }
+}
